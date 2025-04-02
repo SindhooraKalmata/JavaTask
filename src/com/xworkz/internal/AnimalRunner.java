6893900bcd42d6436da03fb36f541addcf625184
SindhooraKalmata <sindhoorakalmata2003@gmail.com>
2025-04-02 22:01:08 +0530

    inheritance

---
+++ src/com/xworkz/internal/AnimalRunner.java
@@ -0,0 +1,13 @@
+package com.xworkz.internal;
+
+public class AnimalRunner {
+    public static void main(String[] args){
+        Horse horse=new Horse();
+        horse.colour();
+        horse.height();
+        horse.age();
+        horse.gender();
+        horse.weight();
+
+    }
+}
